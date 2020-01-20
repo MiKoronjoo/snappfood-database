aa5fa59cfe1f1cc1d8a1dfadef3f3014444f0fb3
Hassan Abbasi <hassan.abbp@gmail.com>
2020-01-20 14:18:02 +0330

    Add attributes to User

--- entities/user.py
+++ entities/user.py
@@ -1,2 +1,9 @@
 class User:
-    pass
+    def __init__(self, userId, first_name, last_name, phone_number, email, password, walletId):
+        self.userId = userId
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+        self.password = password
+        self.walletId = walletId
