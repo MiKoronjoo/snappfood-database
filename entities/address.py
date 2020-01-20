33685006f93b6d6510f34199b2bc8c710b1fece1
Hassan Abbasi <hassan.abbp@gmail.com>
2020-01-20 13:50:01 +0330

    Add attributes to Address

--- entities/address.py
+++ entities/address.py
@@ -1,2 +1,9 @@
 class Address:
-    pass
+    def __init__(self, addressId, userId, cityId, locationId, street=None, alley=None, plaque=None):
+        self.addressId = addressId
+        self.userId = userId
+        self.cityId = cityId
+        self.locationId = locationId
+        self.street = street
+        self.alley = alley
+        self.plaque = plaque
