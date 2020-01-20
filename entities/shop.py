cbdba444446a35b8f29057a5dd91ecbba73a2f8e
Hassan Abbasi <hassan.abbp@gmail.com>
2020-01-20 14:12:35 +0330

    Add attributes to Shop

--- entities/shop.py
+++ entities/shop.py
@@ -1,2 +1,7 @@
 class Shop:
-    pass
+    def __init__(self, shopId, name, addressId, minimum_bill_value, about_text=None):
+        self.shopId = shopId
+        self.name = name
+        self.addressId = addressId
+        self.minimum_bill_value = minimum_bill_value
+        self.about_text = about_text
