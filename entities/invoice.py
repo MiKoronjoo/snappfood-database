e691bfd76d9b20dc09c32762a9f609b662170add
Hassan Abbasi <hassan.abbp@gmail.com>
2020-01-20 14:08:31 +0330

    Add attributes to Invoice | Update database

--- entities/invoice.py
+++ entities/invoice.py
@@ -1,2 +1,8 @@
 class Invoice:
-    pass
+    def __init__(self, invoiceId, addressId, walletId, statusId, commentId, discountId):
+        self.invoiceId = invoiceId
+        self.addressId = addressId
+        self.walletId = walletId
+        self.statusId = statusId
+        self.commentId = commentId
+        self.discountId = discountId
