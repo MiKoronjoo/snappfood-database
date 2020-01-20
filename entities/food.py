c8a4e7c8adcd89785a1e08a35a60c46a693d37dd
Hassan Abbasi <hassan.abbp@gmail.com>
2020-01-20 14:03:13 +0330

    Add attributes to Food

--- entities/food.py
+++ entities/food.py
@@ -1,2 +1,9 @@
 class Food:
-    pass
+    def __init__(self, foodId, name, price, shopId, categoryId, discount, about=None):
+        self.foodId = foodId
+        self.name = name
+        self.price = price
+        self.shopId = shopId
+        self.categoryId = categoryId
+        self.discount = discount
+        self.about = about
