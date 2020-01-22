acba987292f36e0481bb36eea4ced05fcf6716e6
Hassan Abbasi <hassan.abbp@gmail.com>
2020-01-22 12:54:45 +0330

    Handle insert Address

--- entities/address.py
+++ entities/address.py
@@ -1,9 +1,45 @@
-class Address:
+from entities.location import Location
+from entities.entity import Entity
+
+
+class Address(Entity):
     def __init__(self, addressId, userId, cityId, locationId, street=None, alley=None, plaque=None):
         self.addressId = addressId
         self.userId = userId
         self.cityId = cityId
         self.locationId = locationId
-        self.street = street
-        self.alley = alley
-        self.plaque = plaque
+        self._street = street
+        self._alley = alley
+        self._plaque = plaque
+
+    @classmethod
+    def add(cls, userId, cityId, lat, lon):
+        locationId = Location.add(lat, lon)
+        cls.insert_tuple('Address', ['userId', 'cityId', 'locationId'], [userId, cityId, locationId])
+
+    @property
+    def street(self):
+        return self._street
+
+    @street.setter
+    def street(self, value: str):
+        Address.update_tuple('Address', 'street', value,  f'"addressId" = \'{self.addressId}\'')
+        self._street = value
+
+    @property
+    def alley(self):
+        return self._alley
+
+    @alley.setter
+    def alley(self, value: str):
+        Address.update_tuple('Address', 'alley', value, f'"addressId" = \'{self.addressId}\'')
+        self._alley = value
+
+    @property
+    def plaque(self):
+        return self._plaque
+
+    @plaque.setter
+    def plaque(self, value: str):
+        Address.update_tuple('Address', 'plaque', value, f'"addressId" = \'{self.addressId}\'')
+        self._plaque = value
--- entities/location.py
+++ entities/location.py
@@ -1,5 +1,16 @@
-class Location:
+from pprint import pprint
+
+from entities.entity import Entity
+
+
+class Location(Entity):
     def __init__(self, locationId, lat, lon):
         self.locationId = locationId
         self.lat = lat
         self.lon = lon
+
+    @classmethod
+    def add(cls, lat: float, lon: float):
+        cls.insert_tuple('location', ['lat', 'lon'], [str(lat), str(lon)])
+        tbl = cls.select_tuples('location', ['lat', 'lon'], [str(lat), str(lon)])
+        return tbl[-1][2]
